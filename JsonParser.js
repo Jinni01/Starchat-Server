717226b582275018ccfa1fe4147a7a82aaaf2d2d
심의진 <Kusinaga@cpe-172-89-15-182.socal.res.rr.com>
2019-09-07 23:36:31 +0900

    test2

--- JsonParser.js
+++ JsonParser.js
@@ -1,7 +1,5 @@
-const discriminateParse = (jsonData) => {
+exports.discriminateParse = (jsonData) => {
     const parsedData = (typeof (jsonData) == 'string' || jsonData instanceof String) ? JSON.parse(jsonData) : jsonData;
-
+    
     return parsedData;
 }
-
-module.exports = discriminateParse;
