b518ba787ad8698f4c9d17fc7c30262704c8d55d
심의진 <Kusinaga@sim-uijins-MacBook-Air.local>
2019-09-25 09:38:52 +0900

    관리자 페이지 추가

--- routes/admin/admin.js
+++ routes/admin/admin.js
@@ -5,7 +5,7 @@ const passport = require("passport");
 
 const isAuthenticated = (req, res, next) => {
     if (req.isAuthenticated()) {
-        if (req.user.type == 'admin') {
+        if (req.user.type == "admin") {
             return res.status(400).json({
                 success: false,
                 message: "이미 로그인되어 있습니다"
@@ -22,6 +22,18 @@ const isAuthenticated = (req, res, next) => {
 
 router.get("/", (req, res) => {
 
+    if (req.user) {
+        if (req.user.type == "admin") {
+            res.redirect("/admin/menu");
+        } else {
+
+        }
+    } else {
+        res.redirect("/admin/logintab");
+    }
+});
+
+router.get("/logintab", (req, res) => {
     fs.readFile(path.join("static/html/admin/login.html"), (err, data) => {
         if (err) {
             return res.status(500).json({
@@ -37,7 +49,6 @@ router.get("/", (req, res) => {
 
     });
 });
-
 //login router
 router.post("/login", isAuthenticated, (req, res, next) => {
     passport.authenticate('local', (err, user, info) => {
@@ -53,10 +64,38 @@ router.post("/login", isAuthenticated, (req, res, next) => {
                     console.log(err);
                 }
                 console.log(req.user);
-                return res.status(200).json(req.user);
+
+                if (user.type == "admin") {
+                    return res.status(200).send({
+                        success: true,
+                        message: "관리자 인증이 완료되었습니다"
+                    });
+                } else {
+                    return res.status(401).send({
+                        success: false,
+                        message: "관리자 권한이 없는 계정입니다"
+                    });
+                }
             })
         }
     })(req, res, next);
+});
+
+router.get("/menu", (req, res) => {
+    fs.readFile(path.join("static/html/admin/menu.html"), (err, data) => {
+        if (err) {
+            return res.status(500).json({
+                message: "페이지를 로딩하는 동안 오류가 발생하였습니다",
+                err: err
+            });
+        } else {
+            res.writeHead(200, {
+                'Content-Type': 'text/html'
+            });
+            res.end(data);
+        }
+
+    });
 });
 
 module.exports = router;
